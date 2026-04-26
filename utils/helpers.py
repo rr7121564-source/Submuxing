27e99302290c805832313d9c243c7a6534f5b068
rr7121564-source <rr7121564@gmail.com>
2026-04-26 12:05:51 +0530

    Delete utils directory

--- utils/helpers.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-def clean_temp_files(path):
-    try:
-        if os.path.isdir(path): shutil.rmtree(path)
-        elif os.path.exists(path): os.remove(path)
-    except Exception as e:
-        print(f"Cleanup error: {e}")
