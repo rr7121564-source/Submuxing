4c8a51ba6595a025d7e6696f24545e772831d26a
rr7121564-source <rr7121564@gmail.com>
2026-04-27 16:05:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,22 @@ import asyncio
 # Settings
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("ADMIN_ID", 0))
-PORT = int(os.environ.get("PORT", 10000))
+PORT = int(os.environ.get("PORT", 7860))
+
+# GitHub Actions Credentials (For Hardsub & Compress)
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+REPO_NAME = os.getenv("REPO_NAME") # Format: username/reponame
+
+# Pyrogram API (GitHub Worker ke liye)
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH")
 
 # Global Objects
 SESSION_ID = str(uuid.uuid4())[:8]
 active_processes = {}
 global_task_lock = asyncio.Lock()
-EXTRACT_DATA = {} # Extraction session ke liye
+EXTRACT_DATA = {} 
 
-# Nika-main wala Language Map
 LANG_MAP = {
     'eng': 'English', 'hin': 'Hindi', 'ara': 'Arabic', 'fre': 'French',
     'ger': 'German', 'ita': 'Italian', 'jpn': 'Japanese', 'spa': 'Spanish',
